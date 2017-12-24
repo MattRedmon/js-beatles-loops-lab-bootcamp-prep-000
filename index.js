edf3f6a736c42d405ca2e27b4acf75e5564f752a
Matt Redmon <matt.redmon@live.com>
2017-12-24 02:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function theBeatlesPlay(musicians, instrument) {
   arrayPlays = []
-  for (let i = musicians.length; i > 0; i--)
+  for (let i = musicians.length; i > 0; i--) {
     arrayPlays.push(`${musicians.pop} plays ${instrument}`)
+  }
+  return arrayPlays
 }
